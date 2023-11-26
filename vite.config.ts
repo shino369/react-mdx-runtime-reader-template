d24daec830e2baad5df295265876309282a9fb6c
SHINO369 <nakagame02@gmail.com>
2023-11-26 16:26:03 +0900

    fix: try to resolve basename

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react-swc'
-import mdx from "@mdx-js/rollup"
+import mdx from '@mdx-js/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    mdx(),
-    react(),     
-  ],
-  base: process.env.GITHUB_PAGES
-  ? '/react-mdx-runtime-reader-template/' // レポジトリ名を設定
-  : './',
+  plugins: [mdx(), react()],
+  base: '/react-mdx-runtime-reader-template/',
   resolve: {
     alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-},
+  },
 })
